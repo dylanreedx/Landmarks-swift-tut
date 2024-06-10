99e50391fe940eb9b8e2aa033175297953dbefb6
Dylan Reed <dylan.reeder5@gmail.com>
2024-06-10 12:36:02 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dylan Reed on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
